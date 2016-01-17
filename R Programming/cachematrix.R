7d890c18347fe71574f5211344e1c0a38d0f5b8a
vasudev daruvuri <vdaruvuri@gmail.com>
2016-01-16 20:23:02 -0800

    updated cachematrix

--- R Programming/cachematrix.R
+++ R Programming/cachematrix.R
@@ -2,9 +2,9 @@
 ##
 ## Usage example:
 ##
-## > source('cachematrix.R')
-## > m <- makeCacheMatrix(matrix(c(2, 0, 0, 2), c(2, 2)))
-## > cacheSolve(m)
+## source('cachematrix.R')
+## m <- makeCacheMatrix(matrix(c(2, 0, 0, 2), c(2, 2)))
+## cacheSolve(m)
 ## [,1] [,2]
 ## [1,]  0.5  0.0
 ## [2,]  0.0  0.5
